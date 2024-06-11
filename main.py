1ca919dd269d5db7a5ab2dbb02da22fdd08ffd08
irikuz <i-iri.kuz@yandex.ru>
2024-06-11 18:13:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+a = 'wordlength'
+print(len(a)) #Длина слова
+
+
+first_number = 7
+second_number = 4
+summa = first_number + second_number
+diff = first_number - second_number
+print(summa, diff) #Суммы и разности
+
+first_number = 8
+second_number = 9
+third_number = 3
+mean = ((first_number + second_number + third_number)/3)
+print(mean) #Среднее арифметическое
+
+
+first_string = 'Понедельник'
+second_string = 'Вторник'
+print(first_string,', ', second_string) #Простые строчки
+
+
+h = 6
+b = 9
+c = 3
+f = (h*b)+(h*c)
+print((f**3)/2) #Сложная формула
